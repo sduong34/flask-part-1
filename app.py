2df68bad9b2cf8536f93275ff32ef65d941dd039
sduong34 <sarahduong34@gmail.com>
2022-09-06 15:18:29 -0500

    1

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def faq():
     return '<h1>Here are some frequently asked questions</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8000)
